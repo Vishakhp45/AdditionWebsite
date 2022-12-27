5afb7c4442af424b489052aef7486b02b8bc4c74
user <user@DESKTOP-5LN8OOV>
2022-12-27 04:25:38 -0500

    Add project files.

---
+++ Controllers/AddController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace AdditionWebsite.Controllers
+{
+    public class AddController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddDisplay(int a,int b)
+        {
+            int ans = a + b;
+            ViewBag.Ans1 = ans;
+
+            return View();
+        }
+    }
+}
